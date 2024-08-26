38531ddf99664d2a03d8af905565c19fd01cba8e
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-08-26 15:56:03 +0300

    remove unnecessary

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import TodoList from './components/TodoList';
 function App() {
  return (
   <>
-   <Container>
-    <h1 className="text-[30px] font-700 text-center pt-[10px] mb-[20px]">
-     ToDo
-    </h1>
-    <TextField styleCls="mb-[20px]" />
-    <ControlPanel styleCls="mb-[20px]" />
-    <TodoList />
-   </Container>
+   <main>
+    <Container>
+     <h1 className="text-[30px] font-700 text-center pt-[10px] mb-[20px]">
+      ToDo
+     </h1>
+     <TextField styleCls="mb-[20px]" />
+     <ControlPanel styleCls="mb-[20px]" />
+     <TodoList />
+    </Container>
+   </main>
   </>
  );
 }
