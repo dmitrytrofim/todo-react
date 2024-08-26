1c2b59a6f2b6608e530ab7939dc5dc6b7b3c1b26
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-08-26 14:53:03 +0300

    fix style items

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,26 +5,31 @@ function TodoItem({ text, id, complete }: ITodoItem) {
  const classBtn =
   'w-[20px] h-[20px] flex items-center justify-center text-[12px] rounded-[5px] bg-[black] text-[white] font-700';
  const itemCompleteCls = complete ? ' bg-[darkgray] text-[white]' : '';
- const btnCompleteCls = complete ? ' !bg-[white] !text-[#000]' : '';
+ const btnCompleteCls = complete ? ' !text-[lightgray]' : '';
 
  const removeItemStore = useStore((state) => state.removeItem);
  const completeItemStore = useStore((state) => state.completeItem);
 
  return (
-  <li
-   className={
-    'flex items-stretch gap-[10px] border rounded-[5px] p-[5px_10px] mb-[5px]' +
-    itemCompleteCls
-   }
-  >
+  <li className="flex items-center gap-[6px] mb-[5px]">
    <button
     onClick={() => completeItemStore(id)}
     className={classBtn + btnCompleteCls}
    >
     &#10003;
    </button>
-   <p className="grow flex items-center">{text}</p>
-   <button onClick={() => removeItemStore(id)} className={classBtn}>
+   <p
+    className={
+     'grow flex items-center border rounded-[5px] p-[5px_10px]' +
+     itemCompleteCls
+    }
+   >
+    {text}
+   </p>
+   <button
+    onClick={() => removeItemStore(id)}
+    className={classBtn + btnCompleteCls}
+   >
     &#10007;
    </button>
   </li>
