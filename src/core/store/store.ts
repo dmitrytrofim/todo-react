e5e514e7e854f328b94f8df6b850da1bf5b262f4
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2025-03-18 11:43:06 +0300

    add middleware persist

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,22 +1,28 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { IStore } from '@/core/types/store';
 
-const useStore = create<IStore>((set, get) => ({
- todos: [],
- addTodo: (text) =>
-  set((state) => ({
-   todos: [
-    ...state.todos,
-    {
-     id: crypto.randomUUID(),
-     text,
-    },
-   ],
-  })),
- removeTodo: (id) =>
-  set(() => ({
-   todos: get().todos.filter((item) => item.id !== id),
-  })),
-}));
+const useStore = create<IStore>()(
+ persist(
+  (set, get) => ({
+   todos: [],
+   addTodo: (text) =>
+    set((state) => ({
+     todos: [
+      ...state.todos,
+      {
+       id: crypto.randomUUID(),
+       text,
+      },
+     ],
+    })),
+   removeTodo: (id) =>
+    set(() => ({
+     todos: get().todos.filter((item) => item.id !== id),
+    })),
+  }),
+  { name: 'Todos' }
+ )
+);
 
 export default useStore;
